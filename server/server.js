17efc729489f5ed331adcd65c95fc697fae598f6
dogurasu <dugs134@gmail.com>
2021-03-15 23:08:39 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import morgan from 'morgan';
+// import connectDB from './config/db.js.js';
+// import { notFound, errorHandler } from './middleware/errorMiddleware.js.js';
+
+// import API router (routes)
+
+dotenv.config();
+
+// connectDB();
+
+const env = process.env.NODE_ENV;
+const port = process.env.PORT;
+const app = express();
+app.use(express.json()); // hook up express.json middleware to accept JSON data in req.body
+if (env === "development") {
+    // dev gives us http methods, status, etc
+    app.use(morgan('dev')); 
+}
+
+// mount routes
+app.get('/', function (req, res) {
+    res.send("Hello World!");
+})
+
+app.listen(port, function() {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
